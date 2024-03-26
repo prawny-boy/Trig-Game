895be718673a7e7c7501df1e815f8b00e5366137
Sean Chan <sean.chan3@student.education.wa.edu.au>
2024-03-26 21:51:22 +0800

    Added a way to translate the player's coordinates A function to move the coords of a player (either 1 or 2) in both seperate x and y directions. eg. player_x + x_move and player_y + y_move

--- Code/main.py
+++ Code/main.py
@@ -1,11 +1,11 @@
 # imports
-import random, time
+import random, time, sys
 
 # define dictionaries
 from triples_list import *
 player_one = {
   "name": "one",
-  "current": [random.randint(-800, 800), random.randint(-800, 800)], # randomises player location
+  "current": [random.randint(0,0), random.randint(0,0)], # randomises player location
   "distance": 0.0,
   "gradient": 0.0,
   "midpoint": [0, 0],
@@ -90,37 +90,94 @@ def draw_plane(): # redraws the cartesian plane with player points on top.
   y_size = 1600 # size of the grid, -800 to 800
   # something here to print cartesian plane. Pygame?
 
+def update_coords(x_add:int, y_add:int, player_num:int): # Translates the coordinates in a certain amount of x and certain amount of y.
+  if player_num == 1:
+    player_one["current"] = [player_one["current"][0] + x_add, player_one["current"][1] + y_add]
+  elif player_num == 2:
+    player_two["current"] = [player_two["current"][0] + x_add, player_two["current"][1] + y_add]
+  elif player_num == 3: # NPC update as player 3
+    pass
+
 # testing functions
-update_dicts()
-print_stats(player_one)
-print_stats(player_two)
+# update_dicts()
+# print_stats(player_one)
+# print_stats(player_two)
 
 # Main loop
-print("""INPUT RULES:
-1. All moves must be in the form of 'distance<space>direction'.
-2. Distance is the amount of units you want to travel.
-3. Direction is a number from 1 to 8, representing the quadrants of the cartesian plane cut in half.
-  -> Cartesian direction means that 0 degrees is East and the positive direction is counter-clockwise.
-4. Have fun playing a game while learning some math!
-""")
-turn = 1
+print("Hello, welcome to this math game by Sean Chan!")
 while True:
-  move = input().split(" ")
 
-  # Find triple
-  isFound = False
-  for num in range(100): # The 100 here is to ensure a triple is picked.
-    for triple in triples: # This loops through the dictionary with triples and finds the one that has the distance with hypothenuse matching.
-      if triple[2] == (int(move[0]) - num): # This checks for the triple with the distance with subtracting every time it is looped.
-        a = triple[0] # if the triple is matching, save the a and b sides.
-        b = triple[1]
-        isFound = True # allows us to break from the second loop.
-        break
-    if isFound == True: # only occurs if the triple is found
-      break
+  # This is the main (text based) menu of the game
+  menu = True
+  while menu:
+    userInput = input("Enter 'start' to start the game. Enter 'help' for additional commands: ").lower().strip() # inputs a command, removing spaces and making it lowercase.
+    if userInput == "start": # checks what command it is a does it.
+      print("Have fun! Starting...")
+      menu = False
+    elif userInput == "help":
+      print("""
+'help' -> Displays all commands and what they do.
+'start' -> Starts a new round of the game.
+'rules' -> Prints the rules of the game.
+'quit' -> Quits this program.
+'npc' -> adds a npc with a certain difficulty. You are also able to select 3 player or 2 player mode with this.
+""")
+    elif userInput == "rules": # Instructions on the game.
+      print("""
+RULES:
+1. SETUP
+  -> This is a 2 player game.
+  -> Each player is placed on a random point on a cartesian plane. (-800 to 800 both x and y)
+  -> A destination will be also placed at a random point.
+2. AIM
+  -> The aim is to move your player into the destination's personal area of 10 units wide
+  -> Or, move your player into another player's personal space of 10 units wide.
+3. TURNS
+  -> Each player will take turns to move, with player 1 starting first.
+  -> A NPC can also be added in the menu at a certain difficulty if wanted, by using "npc".
+  -> This NPC can either be selected as a third player or a second player to play with by yourself.
+4. MOVEMENT
+  -> You can only move along the hypothenuse of a pythagorean triple.
+  -> You will move by inputting a distance and a direction in the format 'distance<space>direction'
+  -> Distance is the amount of units you want to travel.
+  -> Direction is a number from 1 to 8, representing the quadrants of the cartesian plane cut in half.
+  -> Cartesian direction means that 0 degrees is East and the positive direction is counter-clockwise.
+""")
+    elif userInput == "quit":
+      print("Bye!\n")
+      sys.exit()
+    elif userInput == "npc":
+      print("That has not been completed yet. Please try again in later versions.")
+    else:
+      print("That is invalid. Enter 'help' for commands.")
   
-  # Use direction to move to a coordinate
-  if 
+  # After the menu, this is the game code.
+  input("If you haven't read the rules, it is recommended as you won't know how to play. ")
+  turn = 1
+  gameInProgress = True
+  while gameInProgress:
 
-  print(a, b)
-  turn = 2 if turn == 1 else 1 # This changes the turns between 1 and 2.
+    # Get move from player.
+    move = input(f"Player {turn}! Make your move: ")
+    if "quit" in move.lower(): # just in case player wants to quit.
+      print("Bye!\n")
+      sys.exit()
+    move = move.split(" ")
+    distance = int(move[0])
+    direction = int(move[1])
+
+    # Find triple
+    isFound = False
+    for num in range(100): # The 100 here is to ensure a triple is picked.
+      for triple in triples: # This loops through the dictionary with triples and finds the one that has the distance with hypothenuse matching.
+        if triple[2] == (distance - num): # This checks for the triple with the distance with subtracting every time it is looped.
+          a = triple[0] # if the triple is matching, save the a and b sides.
+          b = triple[1]
+          isFound = True # allows us to break from the second loop.
+          break
+      if isFound == True: # only occurs if the triple is found
+        break
+    
+    # Use direction to move to a coordinate.
+    
+    turn += 1 if turn != 2 else 1 # This changes the turns between 1 and 2.
